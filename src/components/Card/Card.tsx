d24d17fe54c68cfd27d2f956779f38dc402548f6
Tomasz Przyborowski <t.s.przyborowski@gmail.com>
2024-03-24 13:59:04 +0100

    Add basic styles

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import style from "./Card.module.css";
+// import style from "./Card.module.css";
 
 type CardProps = {
   imgSrc: string;
@@ -13,19 +13,19 @@ type CardProps = {
 
 export const Card = ({ imgSrc, name, surname, street, postCode, subRegion, phoneNumber }: CardProps) => {
   return (
-    <div className={style.card}>
-      <img className={style.avatar} src={imgSrc} alt="" />
-      <div className={style["card-info-wrapper"]}>
-        <div className={style["info-wrapper"]}>
-          <h3 className="">{`${name} ${surname}`}</h3>
+    <div className="flex flex-wrap flex-col md:flex-row gap-x-8 gap-y-6 p-4 bg-gray-700 rounded-xl">
+      <img className="w-16 h-16 rounded-full" src={imgSrc} alt="" />
+      <div className="border-l pl-8 border-gray-500">
+        <div className="flex flex-col">
+          <h3 className="font-medium text-lg ">{`${name} ${surname}`}</h3>
         </div>
-        <div className={style["info-wrapper"]}>
+        <div className="flex flex-col">
           <h4>Address:</h4>
           <h4>{`${street}, ${postCode}`}</h4>
           <h4>{street}</h4>
           <h4>{subRegion}</h4>
         </div>
-        <div className={style["info-wrapper"]}>
+        <div className="flex flex-col">
           <h4>{phoneNumber}</h4>
         </div>
       </div>
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,16 +1,18 @@
-import style from "./Menu.module.css";
+// import style from "./Menu.module.css";
 import { menuData } from "./data/menu-data";
 
 export const Menu = () => {
   return (
-    <nav className={style.menu}>
-      <div className={style["menu-wrapper"]}>
-        <div className="logo-wrapper"></div>
-        <div className="menu-nav-wrapper">
-          <ul>
-            {menuData.map(navEl=><li key={navEl.link}>
-              <div className="menu-icon-wrapper">{navEl.icon}</div>
-              <a href={navEl.link}>{navEl.linkName}</a>
+    <nav className="sticky bg-gray-950 w-full">
+      <div className="flex flex-row items-center justify-between px-9 py-4">
+        <div className="logo-wrapper">
+          <span className="text-sm font-semibold text-gray-50 uppercase">Phone book</span>
+        </div>
+        <div className="">
+          <ul className="flex flex-row gap-8">
+            {menuData.map(navEl=><li className="flex gap-2 items-center pr-8 border-gray-500 border-r last:border-r-0 hover:text-white transition-all" key={navEl.link}>
+              <div className="">{navEl.icon}</div>
+              <a className="" href={navEl.link}>{navEl.linkName}</a>
             </li>)}
           </ul>
         </div>
